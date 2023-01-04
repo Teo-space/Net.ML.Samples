58064a941aed86d2df84447ac6ae30957f8e70f3
iTeo <barmin.andrey.nnov@gmail.com>
2023-01-04 10:22:33 +0300

    up

--- Net.ML.Samples/IrisPredictionScenario/Test.cs
+++ Net.ML.Samples/IrisPredictionScenario/Test.cs
@@ -4,62 +4,80 @@ using IrisPredictionScenario;
 
 namespace Net.ML.Samples.IrisPredictionScenario
 {
+
+	public record Key<TSource, TDest>();
+
+	//Dictionary<Key, object> d = new();
+
 	internal class Test : iRunnable
 	{
 		readonly string dataPath = "iris.data";
 		readonly string modelPath = "iris_model.zip";
-
-
 		public void Run()
 		{
-			var mlContext = new MLContext();
+			var predictionEngine = PredictionEngine<IrisData, IrisPrediction>(modelPath, dataPath);
 
-			ITransformer model;
-			if (File.Exists(modelPath)) model = mlContext.Model.Load(modelPath, out var schema);
-			else model = TrainAndSave(mlContext, dataPath, modelPath);
-
-
-			var predictionEngine = mlContext.Model.CreatePredictionEngine<IrisData, IrisPrediction>(model);
-			print($"CreatePredictionEngine", ConsoleColor.DarkGray);
-
-			var prediction = predictionEngine
+			predictionEngine
 			.Predict(new IrisData()
 			{
 				SepalLength = 3.3f,
 				SepalWidth = 1.6f,
 				PetalLength = 0.2f,
 				PetalWidth = 5.1f,
-			})
-			.Print(x => $"Predicted flower type is: {x.PredictedLabels}");
+			}).Print(x => $"Predicted flower type is: {x.PredictedLabels}");
+			predictionEngine
+			.Predict(new IrisData()
+			{
+				SepalLength = 4f,
+				SepalWidth = 1.6f,
+				PetalLength = 0.2f,
+				PetalWidth = 5.1f,
+			}).Print(x => $"Predicted flower type is: {x.PredictedLabels}");
+			predictionEngine
+			.Predict(new IrisData()
+			{
+				SepalLength = 4f,
+				SepalWidth = 3f,
+				PetalLength = 1f,
+				PetalWidth = 1f,
+			}).Print(x => $"Predicted flower type is: {x.PredictedLabels}");
 
 		}
 
-
-
-		static ITransformer TrainAndSave(MLContext mlContext, string dataPath, string savePath)
+		static PredictionEngine<TSrc, TDst> PredictionEngine<TSrc, TDst>(string modelPath, string dataPath)
+			where TSrc : class, new()
+			where TDst : class, new()
 		{
-			var reader = mlContext.Data.CreateTextLoader<IrisData>(separatorChar: ',', hasHeader: true);
-			IDataView trainingDataView = reader.Load(dataPath).Print(x => $"Load IDataView");
+			var mlContext = new MLContext();
 
-			var pipeline =
-				mlContext.Transforms.Conversion
-				.MapValueToKey("Label")
-				.Append(mlContext.Transforms
-				.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
-				.Append(mlContext.MulticlassClassification.Trainers
-				.SdcaNonCalibrated(
-					labelColumnName: "Label",
-					featureColumnName: "Features"))
-				.Append(mlContext.Transforms.Conversion
-				.MapKeyToValue("PredictedLabel"))
-				;
+			ITransformer model;
+			if (File.Exists(modelPath))
+			{
+				model = mlContext.Model.Load(modelPath, out var schema);
+				print($"Loaded");
+			}
+			else
+			{
+				var reader = mlContext.Data.CreateTextLoader<TSrc>(separatorChar: ',', hasHeader: true);
+				IDataView trainingDataView = reader.Load(dataPath).Print(x => $"Load IDataView");
 
-			var model = pipeline.Fit(trainingDataView).Print("Trained");
-			mlContext.Model.Save(model, reader, savePath);
-			print($"Saved");
-
-			return model;
+				model = mlContext.Transforms.Conversion
+					.MapValueToKey("Label")
+					.Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+					.Append(mlContext.MulticlassClassification.Trainers.SdcaNonCalibrated(labelColumnName: "Label", featureColumnName: "Features"))
+					.Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"))
+					.Fit(trainingDataView);
+				print($"Trained");
+				mlContext.Model.Save(model, reader, modelPath);
+				print($"Saved");
+			}
+			var engine = mlContext.Model.CreatePredictionEngine<TSrc, TDst>(model);
+			print($"Engine Created");
+			return engine;
 		}
-
 	}
 }
+
+
+
+
--- Net.ML.Samples/Program.cs
+++ Net.ML.Samples/Program.cs
@@ -4,7 +4,6 @@
 	{
 		static void Main(string[] args)
 		{
-
 			Run<IrisPredictionScenario.Test>();
 		}
 	}
